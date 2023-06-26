4cc9e3a86312eb5e2e3dbaaac888d070f534cbbe
GitRealBro <regime.skylark.0y@icloud.com>
2023-06-26 11:41:17 -0400

    Forgot.

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("h")
+let compChoice = ""
+function getComputerChoice(compChoice) {
+    console.log("h")
+    let randomNumber = (Math.random() * (3 - 1));
+  if (randomNumber == 1) {
+        console.log("h")
+    compChoice = "Scissors"
+} else if (randomNumber == 2) {
+    console.log("h")
+    compChoice = "Rock"
+} else {
+    console.log("h")
+    compChoice = "Paper"
+}
+console.log(randomNumber)
+console.log(compChoice)
+}
+
+console.log(getComputerChoice(compChoice))
